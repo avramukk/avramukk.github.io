927dbe6f957c48bc99d2a2720f3008ba5787c8d5
avramukk <avramuk89@gmail.com>
2024-05-30 14:32:04 +0100

    update

--- public/js/zooming.js
+++ public/js/zooming.js
@@ -25,9 +25,9 @@
     mod
   ));
 
-  // ns-hugo:/Users/kolia/Repos/mykolaavramuk/assets/lib/js/zooming-v2.1.1.min.js
+  // ns-hugo:/Users/kolia/Repos/avramukk.github.io/assets/lib/js/zooming-v2.1.1.min.js
   var require_zooming_v2_1_1_min = __commonJS({
-    "ns-hugo:/Users/kolia/Repos/mykolaavramuk/assets/lib/js/zooming-v2.1.1.min.js"(exports, module) {
+    "ns-hugo:/Users/kolia/Repos/avramukk.github.io/assets/lib/js/zooming-v2.1.1.min.js"(exports, module) {
       !function(t, e) {
         "object" == typeof exports && "undefined" != typeof module ? module.exports = e() : "function" == typeof define && define.amd ? define(e) : (t = t || self).Zooming = e();
       }(exports, function() {
